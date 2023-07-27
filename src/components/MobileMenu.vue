87b86856a93be8537732997f575d8a4eef850825
HtetZarni <97379920+retardH@users.noreply.github.com>
2023-07-27 17:31:57 +0630

    developing home page

--- src/components/MobileMenu.vue
+++ src/components/MobileMenu.vue
@@ -28,30 +28,29 @@ onUpdated(() => {
 </script>
 
 <template>
-  <transition name="mobile-menu">
-    <div v-if="open" class="fixed top-[64px] z-50 w-8/12 min-h-screen bg-blue-950 bg-opacity-95 backdrop-blur-md">
+  <Transition name="mobile-menu" mode="out-in">
+    <div v-if="props.open" class="fixed top-[64px] z-50 w-8/12 bottom-0 bg-blue-950 bg-opacity-95 backdrop-blur-md">
         <div class="flex flex-col gap-4 px-8">
-          <span v-for="item in mobileMenuItems" :key="item.text"
-                class="cursor-pointer hover:text-teal-400 text-blue-50 opacity-100" :class="open === true ?
-                '' :
-                'hidden'"
+          <a v-for="(item,index) in mobileMenuItems" :key="index"
+                class="cursor-pointer hover:text-teal-400 text-blue-50 opacity-100"
+                :class="{hidden: !open, 'font-bold': !open}"
           >
           {{item.text}}
-          </span>
+          </a>
         </div>
     </div>
-  </transition>
+  </Transition>
 </template>
 
 <style>
 .mobile-menu-enter-active,
 .mobile-menu-leave-active {
-  transition: all 0.4s ease;
+  transition: all 0.45s ease;
 }
 
 .mobile-menu-enter-from, .mobile-menu-leave-to {
-  width: 0;
   transform: translateX(-50px);
+  width: 0;
 }
 
 </style>
