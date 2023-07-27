3bfead08ad30d743c387a88b1c5653223bae8303
htetzarni <hzrni.dev@gmail.com>
2023-07-27 23:00:09 +0630

    fixed mobile menu style bug

--- src/components/MobileMenu.vue
+++ src/components/MobileMenu.vue
@@ -1,6 +1,4 @@
 <script setup>
-import {onUpdated} from "vue";
-
 const props = defineProps({
   open: {
     type: Boolean,
@@ -9,35 +7,36 @@ const props = defineProps({
 })
 const mobileMenuItems = [
   {
-    text: 'Home',
+    text: 'Home'
   },
   {
-    text: 'Movies',
+    text: 'Movies'
   },
   {
-    text: 'TV Shows',
+    text: 'TV Shows'
   },
   {
     text: 'People'
   }
 ]
-onUpdated(() => {
-  console.log(props.open);
-})
-
 </script>
 
 <template>
   <Transition name="mobile-menu" mode="out-in">
-    <div v-if="props.open" class="fixed top-[64px] z-50 w-8/12 bottom-0 bg-blue-950 bg-opacity-95 backdrop-blur-md">
-        <div class="flex flex-col gap-4 px-8">
-          <a v-for="(item,index) in mobileMenuItems" :key="index"
-                class="cursor-pointer hover:text-teal-400 text-blue-50 opacity-100"
-                :class="{hidden: !open, 'font-bold': !open}"
-          >
-          {{item.text}}
-          </a>
-        </div>
+    <div
+      v-if="props.open"
+      class="fixed top-0 z-50 w-8/12 bottom-0 bg-blue-950 bg-opacity-95 backdrop-blur-md"
+    >
+      <div class="flex flex-col gap-4 mt-[64px] px-8">
+        <a
+          v-for="(item, index) in mobileMenuItems"
+          :key="index"
+          class="cursor-pointer hover:text-teal-400 text-blue-50 opacity-100"
+          :class="{ hidden: !props.open }"
+        >
+          {{ item.text }}
+        </a>
+      </div>
     </div>
   </Transition>
 </template>
@@ -48,9 +47,19 @@ onUpdated(() => {
   transition: all 0.45s ease;
 }
 
-.mobile-menu-enter-from, .mobile-menu-leave-to {
+.mobile-menu-enter-from,
+.mobile-menu-leave-to {
   transform: translateX(-50px);
   width: 0;
 }
 
+.mobile-menu-enter-from a,
+.mobile-menu-leave-to a {
+  opacity: 0;
+}
+
+.mobile-menu-enter-active a,
+.mobile-menu-leave-active a {
+  transition: all 0.2s ease;
+}
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,45 +1,64 @@
 <template>
-  <div class="bg-blue-950">
-    <nav class="py-4 w-full px-8 gap-6 mx-auto flex items-center max-w-7xl sticky top-0" style="z-index: 999999">
-      <i class="fa-solid fa-bars text-xl text-blue-200 md:hidden" @click="isMobileMenuOpen = !isMobileMenuOpen"></i>
+  <div class="bg-blue-950 sticky top-0" style="z-index: 9999">
+    <nav
+      class="py-4 w-full px-8 gap-6 mx-auto flex items-center max-w-7xl sticky top-0"
+      style="z-index: 999999"
+    >
+      <i
+        class="fa-solid fa-bars text-xl text-blue-200 md:hidden"
+        @click="isMobileMenuOpen = !isMobileMenuOpen"
+      ></i>
       <h3 class="text-teal-400 font-bold text-2xl font-serif mr-auto md:mr-0 md:ml-0">WatchiT</h3>
       <ul class="items-center gap-6 text-blue-50 text-sm hidden md:flex">
         <li class="cursor-pointer hover:text-teal-400">
           <span>Home</span>
         </li>
-        <li class="cursor-pointer hover:text-teal-400 relative" @mouseenter="isMovieDropDownOpen = true" @mouseleave="isMovieDropDownOpen = false">
+        <li
+          class="cursor-pointer hover:text-teal-400 relative"
+          @mouseenter="isMovieDropDownOpen = true"
+          @mouseleave="isMovieDropDownOpen = false"
+        >
           <span>Movies</span>
-          <DropDown title="Movie" :open="isMovieDropDownOpen" :contents="['Popular', 'Now Trending', 'Upcoming',
-          'Top Rated']" />
+          <DropDown
+            title="Movie"
+            :open="isMovieDropDownOpen"
+            :contents="['Popular', 'Now Trending', 'Upcoming', 'Top Rated']"
+          />
         </li>
-        <li class="cursor-pointer relative hover:text-teal-400" @mouseenter="isTvShowDropDownOpen = true" @mouseleave="isTvShowDropDownOpen = false">
+        <li
+          class="cursor-pointer relative hover:text-teal-400"
+          @mouseenter="isTvShowDropDownOpen = true"
+          @mouseleave="isTvShowDropDownOpen = false"
+        >
           <span>TV Shows</span>
-          <DropDown title="Movie" :open="isTvShowDropDownOpen" :contents="['Popular', 'Airing Today', 'On TV',
-          'Top Rated']" />
+          <DropDown
+            title="Movie"
+            :open="isTvShowDropDownOpen"
+            :contents="['Popular', 'Airing Today', 'On TV', 'Top Rated']"
+          />
         </li>
-        <li class="cursor-pointer hover:text-teal-400">
-          People
-        </li>
+        <li class="cursor-pointer hover:text-teal-400">People</li>
       </ul>
       <div class="flex items-center ml-0 md:ml-auto gap-8">
         <i class="fa-solid fa-magnifying-glass text-xl text-blue-200"></i>
         <i class="relative fa-solid fa-bell text-xl text-blue-200">
           <span
-              class="absolute w-4 h-4 flex items-center justify-center -top-1 -right-[10px] bg-teal-500 text-white rounded-full text-[8px]">
+            class="absolute w-4 h-4 flex items-center justify-center -top-1 -right-[10px] bg-teal-500 text-white rounded-full text-[8px]"
+          >
             4
           </span>
         </i>
       </div>
     </nav>
-    <MobileMenu :open="isMobileMenuOpen"/>
+    <MobileMenu :open="isMobileMenuOpen" />
   </div>
 </template>
 
 <script setup>
-import DropDown from "@/components/Common/HoverDropDown.vue";
-import {ref} from "vue";
-import MobileMenu from "@/components/MobileMenu.vue";
-const isMovieDropDownOpen = ref(false);
-const isTvShowDropDownOpen = ref(false);
-const isMobileMenuOpen = ref(false);
+import DropDown from '@/components/Common/HoverDropDown.vue'
+import { ref } from 'vue'
+import MobileMenu from '@/components/MobileMenu.vue'
+const isMovieDropDownOpen = ref(false)
+const isTvShowDropDownOpen = ref(false)
+const isMobileMenuOpen = ref(false)
 </script>
