deb50734696118706343235d058aaa0acb9ecf39
htetzarni <hzrni.dev@gmail.com>
2023-08-04 00:24:21 +0630

    navbar page refactor

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,11 +3,36 @@ import DropDown from '@/components/Common/HoverDropDown.vue'
 import { ref } from 'vue'
 import MobileMenu from '@/components/MobileMenu.vue'
 import Router from '@/router'
-
 const isMovieDropDownOpen = ref(false)
 const isTvShowDropDownOpen = ref(false)
 const isPeopleDropDownOpen = ref(false)
 const isMobileMenuOpen = ref(false)
+const navbarLinks = ref([
+  {
+    text: 'Movies',
+    dropDown: {
+      title: 'Movie',
+      openState: isMovieDropDownOpen,
+      contents: ['Popular', 'Now Playing', 'Upcoming', 'Top Rated']
+    }
+  },
+  {
+    text: 'TV Shows',
+    dropDown: {
+      title: 'TV',
+      openState: isTvShowDropDownOpen,
+      contents: ['Popular', 'Airing Today', 'On The Air', 'Top Rated']
+    }
+  },
+  {
+    text: 'People',
+    dropDown: {
+      title: 'People',
+      openState: isPeopleDropDownOpen,
+      contents: ['Popular']
+    }
+  }
+])
 </script>
 
 <template>
@@ -27,46 +52,21 @@ const isMobileMenuOpen = ref(false)
         WatchiT
       </h3>
       <ul class="items-center gap-6 text-blue-50 text-sm hidden md:flex">
-        <li class="cursor-pointer hover:text-teal-400" @click="Router.push('/')">
-          <span>Home</span>
-        </li>
         <li
+          v-for="link in navbarLinks"
+          :key="link.text"
           class="cursor-pointer hover:text-teal-400 relative"
-          @mouseenter="isMovieDropDownOpen = true"
-          @mouseleave="isMovieDropDownOpen = false"
+          @mouseenter="link.dropDown.openState = true"
+          @mouseleave="link.dropDown.openState = false"
         >
-          <span>Movies</span>
+          <span>
+            {{ link.text }}
+          </span>
           <DropDown
-            title="Movie"
-            :open="isMovieDropDownOpen"
-            :contents="['Popular', 'Now Playing', 'Upcoming', 'Top Rated']"
-            @menu-click="(v) => (isTvShowDropDownOpen = false)"
-          />
-        </li>
-        <li
-          class="cursor-pointer relative hover:text-teal-400"
-          @mouseenter="isTvShowDropDownOpen = true"
-          @mouseleave="isTvShowDropDownOpen = false"
-        >
-          <span>TV Shows</span>
-          <DropDown
-            title="TV"
-            :open="isTvShowDropDownOpen"
-            :contents="['Popular', 'Airing Today', 'On The Air', 'Top Rated']"
-            @menu-click="(v) => (isMovieDropDownOpen = false)"
-          />
-        </li>
-        <li
-          class="cursor-pointer hover:text-teal-400"
-          @mouseenter="isPeopleDropDownOpen = true"
-          @mouseleave="isPeopleDropDownOpen = false"
-        >
-          <span>People</span>
-          <DropDown
-            title="People"
-            :open="isPeopleDropDownOpen"
-            :contents="['Popular']"
-            @menu-click="(v) => (isPeopleDropDownOpen = false)"
+            :title="link.dropDown.title"
+            :open="link.dropDown.openState"
+            :contents="link.dropDown.contents"
+            @menu-click="(v) => (link.dropDown.openState = false)"
           />
         </li>
       </ul>
